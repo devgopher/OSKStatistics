a2f931b79fe0192ee52a77fb9aee9760eaf089f7
devgopher <igevdo@mail.ru>
2021-04-02 19:35:36 +0300

    - sqlite

--- OSK/OrleansStatisticsKeeper.Grains/Database/ConnectionsFactory.cs
+++ OSK/OrleansStatisticsKeeper.Grains/Database/ConnectionsFactory.cs
@@ -1,9 +1,17 @@
-﻿using MongoDB.Driver;
+﻿using Microsoft.Data.Sqlite;
+using MongoDB.Driver;
+using System.Data;
 
 namespace OrleansStatisticsKeeper.Grains.Database
 {
     public static class ConnectionsFactory
     {
         public static MongoClient OpenMongo(string connString) => new MongoClient(connString);
+        public static IDbConnection OpenRdms(string type, string connString)
+            => type.ToLower() switch
+            {
+                "sqlite" => new SqliteConnection(connString),
+                _ => null,
+            };
     }
 }
---
+++ OSK/OrleansStatisticsKeeper.Grains/Utils/RDMSUtils.cs
@@ -0,0 +1,31 @@
+﻿using OrleansStatisticsKeeper.Grains.Database;
+using OrleansStatisticsKeeper.Grains.Models;
+using OrleansStatisticsKeeper.Models.Settings;
+using System.Data;
+using System.Threading.Tasks;
+
+namespace OrleansStatisticsKeeper.Grains.Utils
+{
+    public class RDMSUtils
+    {
+        private readonly OskSettings _settings;
+        private readonly string RdmsType;
+
+        public RDMSUtils(OskSettings settings) => _settings = settings;
+
+        public async Task<DataTable> GetData<T>()
+            where T : DataChunk
+        {
+            using (var rdmsConnection = ConnectionsFactory.OpenRdms(_settings.ConnectionType, _settings.ConnectionString))
+            {
+                rdmsConnection.Open();
+                
+            }
+
+
+            
+   
+            return collection;
+        }
+    }
+}
